fd688a48f97a384a7c3b8c199b718f9c06f3e93a
Severin Simmler <severin.simmler@posteo.de>
2017-10-07 15:26:38 +0200

    Update __init__.py

--- dariah_topics/__init__.py
+++ dariah_topics/__init__.py
@@ -3,8 +3,8 @@ The :mod:`dariah_topics` package currently offers seven modules:
 
 * :mod:`dariah_topics.doclist` for maintaining document lists. 
 * :mod:`dariah_topics.evaluation` for evaluating semantic coherence of topics.
-* :mod:`dariah_topics.preprocessing` for preprocessing text data.
 * :mod:`dariah_topics.postprocessing` for postprocessing text data.
+* :mod:`dariah_topics.preprocessing` for preprocessing text data.
 * :mod:`dariah_topics.mallet` for calling MALLET from within Python.
 * :mod:`dariah_topics.meta` for maintaining metadata.
 * :mod:`dariah_topics.visualization` for visualizing the output of LDA models.
