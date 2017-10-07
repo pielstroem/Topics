3f29940d1cf1d336577f788e3cf1a4825b31ea4e
Severin Simmler <severin.simmler@posteo.de>
2017-10-08 00:01:32 +0200

    Add metadata in __init__.py

--- dariah_topics/__init__.py
+++ dariah_topics/__init__.py
@@ -9,3 +9,11 @@ The :mod:`dariah_topics` package currently offers seven modules:
 * :mod:`dariah_topics.meta` for maintaining metadata.
 * :mod:`dariah_topics.visualization` for visualizing the output of LDA models.
 """
+
+__author__ = "Sina Bock, Philip Dürholt, Michael Huber, Thora Hagen, Severin Simmler, Thorsten Vitt"
+__version__ = "0.3.0dev1"
+__copyright__ = "Copyright 2017, DARIAH-DE"
+__credits__ = ["Sina Bock", "Philip Dürholt", "Michael Huber", "Thora Hagen", "Steffen Pielström", "Severin Simmler", "Thorsten Vitt"]
+__maintainer__ = "Steffen Pielström"
+__email__ = "pielstroem@biozentrum.uni-wuerzburg.de"
+__status__ = "Development"
