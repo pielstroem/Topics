115f4a6004a8e14fcc6146c02d3e16e10042314a
severinsimmler <severin.simmler@posteo.de>
2019-05-11 15:33:54 +0200

    fix: test

--- tests/test_mallet.py
+++ tests/test_mallet.py
@@ -18,3 +18,39 @@ def test_call_error():
         utils.call(["whoami", "-foo"])
     with pytest.raises(FileNotFoundError):
         utils.call(["foo"])
+
+
+EXECUTABLE = "mallet"
+INPUTFILE = Path("test", "document.txt")
+OUTPUTFILE = Path("test", "document.mallet")
+OUTPUTTOPIC = Path("test", "topic.txt")
+
+
+def test_mallet_call():
+    assert core.call(executable=EXECUTABLE,
+                     command="import-file",
+                     filepath=INPUTFILE,
+                     output=OUTPUTFILE)
+    assert OUTPUTFILE.exists()
+    OUTPUTFILE.unlink()
+
+
+def test_mallet_class():
+    mallet = api.MALLET(EXECUTABLE)
+    assert mallet.executable == EXECUTABLE
+    assert mallet.import_file(filepath=INPUTFILE,
+                              output=OUTPUTFILE,
+                              keep_sequence=True)
+    assert OUTPUTFILE.exists()
+    mallet.train_topics(filepath=OUTPUTFILE,
+                        output_topic_keys=OUTPUTTOPIC)
+    assert OUTPUTTOPIC.exists()
+    OUTPUTFILE.unlink()
+    OUTPUTTOPIC.unlink()
+
+
+def test_mallet_exception():
+    with pytest.raises(FileNotFoundError):
+        mallet = api.MALLET("does-not-exist")
+        mallet.import_file(filepath=INPUTFILE,
+                           output=OUTPUTFILE)
